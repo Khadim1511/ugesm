2d8573d519928effb39b4d2ec166e429fb2ac6c6
Khadim1511 <89653168+Khadim1511@users.noreply.github.com>
2024-12-07 12:24:21 +0100

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'UGESM - Union Générale des Étudiants et Stagiaires Marocains',
+  title: 'UGESM - Union Générale des Étudiants et Stagiaires sénégalais Marocains',
   description: 'Site officiel de l\'UGESM',
 }
 
